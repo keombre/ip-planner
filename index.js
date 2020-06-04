7f5ad8878c17a40cff56d19fccf32c59907c9f6f
keombre <keombre8@gmail.com>
2020-06-04 15:01:53 +0200

    working js

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ class IP {
         this.p2 = p2;
         this.p3 = p3;
         this.p4 = p4;
+
+        this.bitMap = (p1 << 24) + (p2 << 16) + (p3 << 8) + p4;
+    }
+    
+    static FromBitMap(bitMap) {
+        return new IP(bitMap >> 24, (bitMap >> 16) & 255, (bitMap >> 8) & 255, bitMap & 255);
+    }
+
+    MaskPrefix(mask) {
+        let bitMap = (this.bitMap >> mask.Offset()) << mask.Offset();
+        return IP.FromBitMap(bitMap);
+    }
+
+    toString() {
+        return `${this.p1}.${this.p2}.${this.p3}.${this.p4}`;
+    }
+
+    equals(ip) {
+        return this.bitMap == ip.bitMap
     }
 }
 
@@ -11,12 +30,28 @@ class Mask {
     constructor(prefix) {
         this.prefix = prefix;
     }
+
+    Offset() {
+        return 32 - this.prefix;
+    }
+
+    toString() {
+        return `${this.prefix}`;
+    }
 }
 
 class SubNet {
     constructor(address, mask) {
         this.address = address;
         this.mask = mask;
+    }
+
+    Broadcast() {
+        return IP.FromBitMap(this.address.bitMap | ((1 << this.mask.Offset()) - 1));
+    }
+
+    toString() {
+        return `${this.address}/${this.mask}`;
     }
 }
 
@@ -33,7 +68,18 @@ class AddressSpace {
     }
 
     AddSubNet(count) {
-        mask = this.GetMaskForCount(count);
+        if (count < 2)
+            count = 2;
+        let mask = this.GetMaskForCount(count);
+        this.FindFreeAddress(mask);
+    }
+
+    static GetNextSubNet(subNet, mask) {
+        let address = IP.FromBitMap(subNet.Broadcast().bitMap + 1);
+        if (address.MaskPrefix(mask).equals(address))
+            return new SubNet(address, mask);
+        let ip = IP.FromBitMap(address.MaskPrefix(mask).bitMap + (1 << mask.Offset()));
+        return new SubNet(ip, mask);
     }
 
     FindFreeAddress(mask) {
@@ -41,9 +87,28 @@ class AddressSpace {
             this.subnets.push(new SubNet(this.address, mask));
             return;
         }
-        // get size of empty block
-        for(i = 0; i < this.subnets.length; i++) {
-            this.subnets[i]
+
+        for(let i = 0; i < this.subnets.length; i++) {
+            let nextSubNet = AddressSpace.GetNextSubNet(this.subnets[i], mask);
+            if (i + 1 == this.subnets.length) {
+                this.subnets.push(nextSubNet);
+                return;
+            }
+
+            if (nextSubNet.Broadcast().bitMap + 1 <= this.subnets[i+1].address.bitMap) {
+                this.subnets.splice(i+1, 0, nextSubNet);
+                return;
+            }
         }
     }
+
+    toString() {
+        let ret = "";
+        this.subnets.forEach(e => ret += e.toString() + " B: " + e.Broadcast() + "\n");
+        return ret;
+    }
+}
+
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
 }
